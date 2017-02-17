2814ecb6c733cb036f53451967dbdc8a205ba402
qin xiaocha <18577272022@163.com>
2017-02-17 17:36:36 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path:  __dirname + '/',
+    publicPath: './dist/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      }
+    ]
+  },
+  vue: {
+    loaders: {
+      js: 'babel'
+    }
+  }
+}
